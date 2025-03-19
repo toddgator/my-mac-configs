908716c25c02f6e47d680a4ef1a6b5165ba31dff
Todd Smith <tsmith@thig.com>
2025-03-18 23:53:06 -0400

    Adding tmux, ghostty, and oh-my-zsh configs.

---
+++ oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,3 @@
+
+alias l="ls -lh"
+alias vim="nvim"
---
+++ oh-my-zsh/custom/exports.zsh
@@ -0,0 +1,6 @@
+
+export JAVA_HOME="$HOME/Java/jdk-11.0.26+4/Contents/Home"
+export PATH="$JAVA_HOME/bin:$PATH"
+
+export M2_HOME="/Applications/IntelliJ\ IDEA.app/Contents/plugins/maven/lib/maven3"
+export PATH="$M2_HOME/bin:$PATH"
